d835630b63abbf9503f40cca827967c0dd50a9a8
Yuta2049 <vinci2049@gmail.com>
2018-11-08 15:38:32 +0400

    Some commit added in Idea

--- src/Starter.java
+++ src/Starter.java
@@ -4,6 +4,8 @@ import demoService.DemoServiceImpl;
 public class Starter {
     public static void main(String[] args) {
 
+        // Added some comment in Idea
+        
         DemoServiceImpl demoServiceImpl = new DemoServiceImpl();
         demoServiceImpl.showDemo();
 
