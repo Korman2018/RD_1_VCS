1923357eaef768d076945c7afa8ae8608a407c76
Yuta2049 <rendalina@rambler.ru>
2018-11-08 15:36:48 +0400

    Update Starter.java

--- src/Starter.java
+++ src/Starter.java
@@ -4,6 +4,9 @@ import demoService.DemoServiceImpl;
 public class Starter {
     public static void main(String[] args) {
 
+        
+        // Added some comments to this code.
+        
         DemoServiceImpl demoServiceImpl = new DemoServiceImpl();
         demoServiceImpl.showDemo();
 
