21619d83e3f1fc3c07af2489919d210ac1e615f1
Yuta2049 <vinci2049@gmail.com>
2018-11-12 10:53:47 +0400

    Changes after Cherry pick

--- src/data/Discipline.java
+++ src/data/Discipline.java
@@ -5,6 +5,8 @@ public class Discipline {
 
     // Some changes in the branch Master in Idea
 
+    // Some changes in branch Feature_1 in Idea
+
     // Some changes in Develop branch
 
     private String name;
